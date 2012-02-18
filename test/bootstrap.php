a39acd82bfb5fea6e1f8dd46a0509b5543989191
Robert Hafner <tedivm@tedivm.com>
2012-02-18 15:44:07 -0800

    Changed the Autoloader's filename
    Dropped the .class part of the autoloader filename and updated the test
    suite's bootstrap file to reflect that.

--- test/bootstrap.php
+++ test/bootstrap.php
@@ -4,6 +4,6 @@ define('TESTING', true); // this is basically used by the StashArray handler to 
 						// use this anywhere else in the project since that would defeat the point of testing.
 error_reporting(-1);
 $currentDir = dirname(__FILE__) . '/';
-include($currentDir . '../lib/Stash/Autoloader.class.php');
+include($currentDir . '../lib/Stash/Autoloader.php');
 Stash\Autoloader::register();
 ?>
