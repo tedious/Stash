87cedd3a3ce23f0934c907c643aaaf0093ed8e52
Robert Hafner <tedivm@tedivm.com>
2012-07-08 20:04:17 -0700

    Reorganized the class, added functionality for options

--- src/Stash/Session.php
+++ src/Stash/Session.php
@@ -21,13 +21,39 @@ use Stash\Pool;
 class SessionHandler implements SessionHandlerInterface
 {
     protected $pool;
-    protected $namespace;
+    protected $path;
+    protected $options = array();
     
     public function __construct(Pool $pool)
     {
         $this->pool = $pool;
+        $this->options['ttl'] = (int) ini_get('session.gc_maxlifetime');        
+    }
+    
+    public function setOptions($options = array())
+    {
+        $this->options = array_merge_recursive($this->options, $options);
+    }
+    
+    public function open($save_path, $session_id)
+    {
+        $this->path = md5($save_path) . '/';
+        return true;
+    }
+    
+    public function read($session_id)
+    {
+        $cache = $this->pool->getCache($this->path . $session_id);
+        return (!$cache->isMiss()) ? $cache->get() : '';
     }
 
+    public function write($session_id, $session_data)
+    {
+        $id = $this->path . $session_id;
+        $cache = $this->pool->getCache($this->path . $session_id);
+        return $cache->store($session_data, $this->options['ttl']);
+    }
+    
     public function close()
     {
         return true;
@@ -35,31 +61,13 @@ class SessionHandler implements SessionHandlerInterface
 
     public function destroy($session_id)
     {
-        $cache = $this->pool->getCache($session_id);
+        $cache = $this->pool->getCache($this->path . $session_id);
         return $cache->clear();
     }
 
     public function gc($maxlifetime)
     {
         return $this->pool->purge();
-    }
-
-    public function open($save_path, $session_id)
-    {
-        $this->namespace = md5($save_path);
-        return true;
-    }
-
-    public function read($session_id)
-    {
-        $cache = $this->pool->getCache($session_id);
-        return (!$cache->isMiss()) ? $cache->get($session_data) : '';
-    }
-
-    public function write($session_id, $session_data)
-    {
-        $cache = $this->pool->getCache($session_id);
-        return $cache->store($session_data);
     }
 
 }
