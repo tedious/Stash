08f5b207af3fc784df48f9c58c9698bdbef5ed78
Phillip Whelan <pwhelan@mixxx.org>
2013-02-19 01:59:06 -0800

    Add test to check that directories are created using the key hash function.

--- tests/Stash/Test/Driver/FileSystemTest.php
+++ tests/Stash/Test/Driver/FileSystemTest.php
@@ -12,6 +12,12 @@
 namespace Stash\Test\Driver;
 
 use Stash\Driver\FileSystem;
+use Stash\Item;
+
+function strdup($str)
+{
+    return $str;
+}
 
 /**
  * @package Stash
@@ -37,5 +43,40 @@ class FileSystemTest extends AbstractDriverTest
     public function testOptionKeyHashFunction()
     {
         $driver = new FileSystem(array('keyHashFunction' => 'md5'));
+    }
+    
+    /**
+     * Test that the paths are created using the key hash function.
+     */
+    public function testOptionKeyHashFunctionDirs()
+    {
+        $hashfunctions = array('Stash\Test\Driver\strdup', 'strrev', 'md5');
+        $paths = array('one', 'two', 'three', 'four');
+        
+        
+        foreach($hashfunctions as $hashfunction) {
+            
+            $driver = new FileSystem(array(
+                'keyHashFunction' => $hashfunction,
+                'path' => sys_get_temp_dir().DIRECTORY_SEPARATOR.'stash',
+                'dirSplit' => 1
+            ));
+            
+            $rand = str_repeat(uniqid(), 32);
+            
+            $item = new Item($driver, $paths);
+            $item->set($rand);
+            
+            $allpaths = array_merge(array('cache'), $paths);
+            $predicted = sys_get_temp_dir().
+                            DIRECTORY_SEPARATOR.
+                            'stash'.
+                            DIRECTORY_SEPARATOR.
+                            implode(DIRECTORY_SEPARATOR, 
+                                array_map($hashfunction, $allpaths)).
+                            '.php';
+            
+            $this->assertFileExists($predicted);
+        }
     }
 }
