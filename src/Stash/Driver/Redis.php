b9fbbfc046f2fa14365694718bd3aeaa08227f79
Robert Hafner <tedivm@tedivm.com>
2013-06-17 01:34:09 -0700

    Fleshed out the Redis driver.
    This fleshes out the driver a bit more. Right now some functionality is
    working, but some is not- getting and storing data works for some
    datatypes, for instance, but clearing does not quite work.

--- src/Stash/Driver/Redis.php
+++ src/Stash/Driver/Redis.php
@@ -22,6 +22,9 @@ use Stash;
  */
 class Redis implements DriverInterface
 {
+    protected $defaultOptions = array ();
+    protected $redis;
+    protected $keyCache = array();
 
     /**
      *
@@ -29,6 +32,58 @@ class Redis implements DriverInterface
      */
     public function __construct(array $options = array())
     {
+       if(!self::isAvailable())
+           throw new \RuntimeException('Unable to load Redis driver without PhpRedis extension.');
+
+        // Normalize Server Options
+        if(isset($options['servers']))
+        {
+            $servers = (is_array($options['servers']))
+                ? $options['servers']
+                : array($options['servers']);
+
+            unset($options['servers']);
+
+        }else{
+            $servers = array(array('host' => '127.0.0.1', 'port' => '6379', 'ttl' => 0.1));
+        }
+
+        // Merge in default values.
+        $options = array_merge($this->defaultOptions, $options);
+
+
+
+
+        // this will have to be revisited to support multiple servers, using
+        // the RedisArray object. That object acts as a proxy object, meaning
+        // most of the class will be the same even after the changes.
+
+
+        $server = $servers[0];
+
+        $redis = new \Redis();
+
+
+
+        if(isset($server['socket'])) {
+            $redis->connect($server['socket']);
+        }else{
+            $port = isset($server['port']) ? $server['port'] : 6369;
+            $ttl = isset($server['ttl']) ? $server['ttl'] : 0.1;
+            $redis->connect($server['host'], $port, $ttl);
+        }
+
+        // auth - just password
+        if(isset($options['password']))
+            $redis->auth($options['password']);
+
+
+        // select database
+        if(isset($options['database']))
+            $redis->select($options['database']);
+
+        $this->redis = $redis;
+
 
     }
 
@@ -40,7 +95,7 @@ class Redis implements DriverInterface
      */
     public function getData($key)
     {
-        return array('data' => $data, 'expiration' => $expiration);
+        return unserialize($this->redis->get($this->makeKeyString($key)));
     }
 
     /**
@@ -53,7 +108,13 @@ class Redis implements DriverInterface
      */
     public function storeData($key, $data, $expiration)
     {
-
+        $store = serialize(array('data' => $data, 'expiration' => $expiration));
+        if(is_null($expiration))
+        {
+            return $this->redis->setex($this->makeKeyString($key), $store, $expiration);
+        }else{
+            return $this->redis->set($this->makeKeyString($key), $store);
+        }
     }
 
     /**
@@ -65,7 +126,18 @@ class Redis implements DriverInterface
      */
     public function clear($key = null)
     {
+        if(is_null($key))
+        {
+            $this->redis->flushDB();
+            return true;
+        }
 
+        $keyString = $this->makeKeyString($key);
+        $keyReal = $this->makeKeyString($key);
+        $this->redis->incr($keyString); // increment index for children items
+        $this->redis->delete($keyReal); // remove direct item.
+        $this->keyCache = array();
+        return true;
     }
 
     /**
@@ -74,6 +146,8 @@ class Redis implements DriverInterface
      */
     public function purge()
     {
+        // @todo when the RedisArray class is used run the rehash function here
+
         return true;
     }
 
@@ -86,5 +160,40 @@ class Redis implements DriverInterface
     {
         return class_exists('Redis', false);
     }
+
+
+    protected function makeKeyString($key, $path = false)
+    {
+        // array(name, sub);
+        // a => name, b => sub;
+
+        $key = \Stash\Utilities::normalizeKeys($key);
+
+        $keyString = 'cache:::';
+        foreach ($key as $name) {
+            //a. cache:::name
+            //b. cache:::name0:::sub
+            $keyString .= $name;
+
+            //a. :pathdb::cache:::name
+            //b. :pathdb::cache:::name0:::sub
+            $pathKey = ':pathdb::' . $keyString;
+            $pathKey = md5($pathKey);
+
+            if (isset($this->keyCache[$pathKey])) {
+                $index = $this->keyCache[$pathKey];
+            } else {
+                $index = $this->redis->incr($pathKey);
+                $this->keyCache[$pathKey] = $index;
+            }
+
+            //a. cache:::name0:::
+            //b. cache:::name0:::sub1:::
+            $keyString .= '_' . $index . ':::';
+        }
+
+        return $path ? $pathKey : md5($keyString);
+    }
+
 
 }
