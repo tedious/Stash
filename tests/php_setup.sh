d07cbd4b1bf2359ed1a7cf4d1933797f4520e37c
Robert Hafner <tedivm@tedivm.com>
2013-06-18 16:24:38 -0700

    Cleaned up the php extension install script with notifications.

--- tests/php_setup.sh
+++ tests/php_setup.sh
@@ -2,11 +2,18 @@
 
 set -e
 
-echo "Install phpredis extension."
+echo "******************************"
+echo "Installing phpredis extension."
+echo "******************************"
+echo ""
+echo ""
+echo "Downloading..."
 git clone git://github.com/nicolasff/phpredis.git
+echo "Configuring..."
 cd phpredis
 phpize
 ./configure
+echo "Installing..."
 make
 make install
 cd ..
