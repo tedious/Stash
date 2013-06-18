1c0a2ae5fbe10c5525284ce1788e5f7bfc8379ee
Robert Hafner <tedivm@tedivm.com>
2013-06-18 15:44:47 -0700

    Tweaked the php_setup.sh script for readability

--- tests/php_setup.sh
+++ tests/php_setup.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-echo "Install phpredis extension." \
-    && git clone git://github.com/nicolasff/phpredis.git \
-    && cd phpredis \
-    && phpize \
-    && ./configure \
-    && make \
-    && make install \
-    && cd .. \
-    && echo "Finished installing phpredis extension."
+set -e
+
+echo "Install phpredis extension."
+git clone git://github.com/nicolasff/phpredis.git
+cd phpredis
+phpize
+./configure
+make
+make install
+cd ..
+echo "Finished installing phpredis extension."
