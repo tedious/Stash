be1bf56d1cb819b08ff444d8f1239829ceebe7e2
Robert Hafner <tedivm@tedivm.com>
2013-07-21 20:46:53 -0700

    Added Driver Extension Interfaces
    These interefaces can be used to add driver level support for features
    the backend system supports. This way behavior that is emulated in
    Stash can be offloaded to that (probably more efficient) sysem.

---
+++ src/Stash/Interfaces/Drivers/ExtendInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * This file is part of the Stash package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Stash\Interfaces\Drivers;
+
+interface ExtendInterface
+{
+    public function extend($ttl);
+}
