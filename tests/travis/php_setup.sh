0d625033891345b7b8f42332b11ee4ab1cc41ed5
Robert Hafner <tedivm@tedivm.com>
2013-12-19 11:28:47 -0800

    Updated travis setup script to not install extensions with the hhvm system.

--- tests/travis/php_setup.sh
+++ tests/travis/php_setup.sh
@@ -2,19 +2,23 @@
 
 set -e
 
-echo "******************************"
-echo "Installing phpredis extension."
-echo "******************************"
-echo ""
-echo ""
-echo "Downloading..."
-git clone git://github.com/nicolasff/phpredis.git
-echo "Configuring..."
-cd phpredis
-phpize
-./configure
-echo "Installing..."
-make
-make install
-cd ..
-echo "Finished installing phpredis extension."
+if which phpize > /dev/null; then
+    echo "******************************"
+    echo "Installing phpredis extension."
+    echo "******************************"
+    echo ""
+    echo ""
+    echo "Downloading..."
+    git clone git://github.com/nicolasff/phpredis.git
+    echo "Configuring..."
+    cd phpredis
+    phpize
+    ./configure
+    echo "Installing..."
+    make
+    make install
+    cd ..
+    echo "Finished installing phpredis extension."
+else
+    echo "Unable to install php extensions on current system"
+fi
