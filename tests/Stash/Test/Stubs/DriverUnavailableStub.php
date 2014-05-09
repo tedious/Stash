6f40ef474a0d998126129d7bf6d546a2f2565928
Robert Hafner <tedivm@tedivm.com>
2014-05-09 00:29:54 -0700

    Added "Unavailable" Driver stub for testing

---
+++ tests/Stash/Test/Stubs/DriverUnavailableStub.php
@@ -0,0 +1,74 @@
+<?php
+
+/*
+ * This file is part of the Stash package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Stash\Test\Stubs;
+
+use Stash;
+use Stash\Test\Exception\TestException;
+use Stash\Interfaces\DriverInterface;
+
+/**
+ * DriverExceptionStub is used for testing how Stash reacts to thrown errors. Every function but the constructor throws
+ * an exception.
+ *
+ * @package Stash
+ * @author  Robert Hafner <tedivm@tedivm.com>
+ *
+ * @codeCoverageIgnore
+ */
+class DriverUnavailableStub implements DriverInterface
+{
+    protected $store = array();
+
+    public function __construct(array $options = array())
+    {
+
+    }
+
+    public function __destruct()
+    {
+    }
+
+    public function getData($key)
+    {
+        throw new TestException('Test exception for ' . __FUNCTION__ . ' call');
+    }
+
+    protected function getKeyIndex($key)
+    {
+        throw new TestException('Test exception for ' . __FUNCTION__ . ' call');
+    }
+
+    public function storeData($key, $data, $expiration)
+    {
+        throw new TestException('Test exception for ' . __FUNCTION__ . ' call');
+    }
+
+    public function clear($key = null)
+    {
+        throw new TestException('Test exception for ' . __FUNCTION__ . ' call');
+    }
+
+    public function purge()
+    {
+        throw new TestException('Test exception for ' . __FUNCTION__ . ' call');
+    }
+
+    public function canEnable()
+    {
+        return false;
+    }
+
+    public static function isAvailable()
+    {
+        return false;
+    }
+}
