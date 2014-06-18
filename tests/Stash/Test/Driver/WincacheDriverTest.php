1fd3dbdb93df196a2f0df03319da29118c71f9eb
vincent.lark@gmail.com <vincent.lark@gmail.com>
2014-06-18 12:32:46 +0200

    Wincache driver support

---
+++ tests/Stash/Test/Driver/WincacheDriverTest.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * This file is part of the Stash package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Stash\Test\Driver;
+
+/**
+ * @package Stash
+ * 
+ * @author  Robert Hafner <tedivm@tedivm.com>
+ */
+class WincacheTest extends AbstractDriverTest
+{
+    protected $driverClass = 'Stash\Driver\Wincache';
+
+    public function testSetOptions()
+    {
+        $driverType = $this->driverClass;
+        $options = $this->getOptions();
+        $options['namespace'] = 'namespace_test';
+        $options['ttl'] = 15;
+        $driver = new $driverType();
+        $driver->setOptions($options);
+
+        $this->assertAttributeEquals('namespace_test', 'wincacheNamespace', $driver, 'Wincache is setting supplied namespace.');
+        $this->assertAttributeEquals(15, 'ttl', $driver, 'Wincache is setting supplied ttl.');
+
+        return parent::testSetOptions();
+    }
+
+}
