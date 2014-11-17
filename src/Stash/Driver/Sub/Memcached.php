88b528ccd13f7279d4d8c2d986f6aaf3a42b5219
Ben Clark <ben.clark@secretsales.com>
2014-11-17 16:43:22 +0000

    Add CLIENT_MODE option
    AWS have a special memcached client that allows for autodiscovery. It
    has an extra option OPT_CLIENT_MODE.
    
    See http://docs.aws.amazon.com/AmazonElastiCache/latest/UserGuide/AutoDiscovery.html
    
    Because the option handling code checks that the option is actually
    defined before setting it, adding this option causes no issues for non
    AWS clients.

--- src/Stash/Driver/Sub/Memcached.php
+++ src/Stash/Driver/Sub/Memcached.php
@@ -59,7 +59,8 @@ class Memcached
                             'RECV_TIMEOUT',
                             'POLL_TIMEOUT',
                             'CACHE_LOOKUPS',
-                            'SERVER_FAILURE_LIMIT'
+                            'SERVER_FAILURE_LIMIT',
+                            'CLIENT_MODE'
         );
 
         $memcached = new \Memcached();
