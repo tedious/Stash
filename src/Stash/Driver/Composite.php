6d3ea10eb2e6923cd88af0caf521ee3199123da8
Steve Clay <steve@mrclay.org>
2014-11-22 10:35:33 -0500

    Help static analysis in composite driver

--- src/Stash/Driver/Composite.php
+++ src/Stash/Driver/Composite.php
@@ -79,6 +79,8 @@ class Composite implements DriverInterface
         foreach ($this->drivers as $driver) {
             if ($return = $driver->getData($key)) {
                 $failedDrivers = array_reverse($failedDrivers);
+                /* @var DriverInterface[] $failedDrivers */
+
                 foreach ($failedDrivers as $failedDriver) {
                     $failedDriver->storeData($key, $return['data'], $return['expiration']);
                 }
@@ -134,6 +136,8 @@ class Composite implements DriverInterface
     protected function actOnAll($action, $args = array())
     {
         $drivers = array_reverse($this->drivers);
+        /* @var DriverInterface[] $drivers */
+
         $return = true;
         $results = false;
         foreach ($drivers as $driver) {
