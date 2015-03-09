3b83c9f8fecbec99796feccccde871e538724f61
Robert Hafner <tedivm@tedivm.com>
2015-03-08 22:29:02 -0700

    Exclude reds extension from php7 testing

--- tests/travis/php_setup.sh
+++ tests/travis/php_setup.sh
@@ -9,7 +9,7 @@ echo "**************************"
 echo ""
 echo "PHP Version: $TRAVIS_PHP_VERSION"
 
-if [ "$TRAVIS_PHP_VERSION" = "hhvm" ] || [ "$TRAVIS_PHP_VERSION" = "hhvm-nightly" ]; then
+if [ "$TRAVIS_PHP_VERSION" = "hhvm" ] || [ "$TRAVIS_PHP_VERSION" = "hhvm-nightly" ] || [ "$TRAVIS_PHP_VERSION" = "7.0" ]; then
     echo "Unable to install php extensions on current system"
 
 else
