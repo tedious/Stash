05586c6703975e9a7ca0f3a756ac6d6295d3e449
Robert Hafner <tedivm@tedivm.com>
2015-12-07 22:56:35 -0800

    Lock apache version for php < 7

--- tests/travis/php_setup.sh
+++ tests/travis/php_setup.sh
@@ -63,7 +63,7 @@ else
         echo "******************************"
         set +e
         pecl config-set preferred_state beta
-        printf "yes\n" | pecl install apcu
+        printf "yes\n" | pecl install apcu-4.0.10
         set -e
         echo "Finished installing apcu-beta extension."
     fi
