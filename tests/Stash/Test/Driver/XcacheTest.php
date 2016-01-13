b1f4eb146ff1b0ffea9cc382a2b01674ce4d7b54
mskoblar-tp <matesko@gmail.com>
2016-01-13 11:17:58 +0100

    xcache driver added

---
+++ tests/Stash/Test/Driver/XcacheTest.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Stash package.
+ *
+ * (c) Robert Hafner <tedivm@tedivm.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Stash\Test\Driver;
+
+/**
+ * @package Stash
+ * @author  Robert Hafner <tedivm@tedivm.com>
+ */
+class XcacheTest extends AbstractDriverTest
+{
+    protected $driverClass = 'Stash\Driver\Xcache';
+
+    public function testSetOptions()
+    {
+        $driverType = $this->driverClass;
+        $options = $this->getOptions();
+        $options['namespace'] = 'namespace_test';
+        $options['ttl'] = 15;
+        $driver = new $driverType();
+        $driver->setOptions($options);
+
+        $this->assertAttributeEquals('namespace_test', 'xcacheNamespace', $driver, 'xcache is setting supplied namespace.');
+        $this->assertAttributeEquals(15, 'ttl', $driver, 'xcache is setting supplied ttl.');
+
+        return parent::testSetOptions();
+    }
+}
