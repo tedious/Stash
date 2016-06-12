d8a58f4f81fa520a7953a63c1053196721edc92d
Phy <i@phy25.com>
2016-06-12 15:14:36 +0800

    Fully fix undefined class issue

--- src/Stash/Item.php
+++ src/Stash/Item.php
@@ -614,7 +614,7 @@ class Item implements ItemInterface
                 break;
 
             case Invalidation::OLD:
-                $this->isHit = $record && $record['data']['return'] !== null;
+                $this->isHit = isset($record['data']) && $record['data']['return'] !== null;
                 break;
 
             default:
