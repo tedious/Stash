2059c34cdfa4ecfab314b0382a86db574ed9b43a
Robert Hafner <tedivm@tedivm.com>
2017-12-07 19:46:15 -0800

    Update extension installation scripts for travis-ci

--- tests/travis/php_setup.sh
+++ tests/travis/php_setup.sh
@@ -9,66 +9,68 @@ echo "**************************"
 echo ""
 echo "PHP Version: $TRAVIS_PHP_VERSION"
 
-if [ "$TRAVIS_PHP_VERSION" = "hhvm" ] || [ "$TRAVIS_PHP_VERSION" = "hhvm-nightly" ]; then
-    echo "Unable to install php extensions on current system"
 
-elif [ "$TRAVIS_PHP_VERSION" = "7.0" ]; then
-
-    echo ""
-    echo "******************************"
-    echo "Installing apcu extension"
-    echo "******************************"
-    set +e
-    printf "yes\n" | pecl install apcu
-    set -e
-    echo "Finished installing apcu-beta extension."
-
-else
-
-    echo ""
-    echo "******************************"
-    echo "Installing phpredis extension."
-    echo "******************************"
-    echo ""
-    echo ""
-    echo "Downloading..."
-    git clone git://github.com/nicolasff/phpredis.git
-    echo "Configuring..."
-    cd phpredis
-    phpize
-    ./configure
-    echo "Installing..."
-    make
-    make install
-    cd ..
-    rm -Rf phpredis
-    echo "Finished installing phpredis extension."
-
-    echo ""
-    echo "******************************"
-    echo "Installing uopz extension if possible (PHP >=5.4)."
-    echo "******************************"
-    set +e
-    pecl install uopz
-    set -e
-    echo "Finished installing uopz extension."
+echo ""
+echo "******************************"
+echo "Installing apcu extension"
+echo "******************************"
+set +e
+printf "yes\n" | pecl install apcu
+set -e
+echo "Finished installing apcu extension."
 
 
-    if [ "$TRAVIS_PHP_VERSION" != "5.4" ]
-    then
-        echo ""
-        echo "******************************"
-        echo "Installing apcu extension"
-        echo "******************************"
-        set +e
-        printf "yes\n" | pecl install apcu-4.0.8
-        set -e
-        echo "Finished installing apcu-beta extension."
-    fi
+echo ""
+echo "******************************"
+echo "Installing memcache extension"
+echo "******************************"
+set +e
+printf "yes\n" | pecl install memcache
+set -e
+echo "Finished installing memcache extension."
 
-fi
 
-if [ -f "tests/travis/php_extensions_${TRAVIS_PHP_VERSION}.ini" ]
+echo ""
+echo "******************************"
+echo "Installing memcached extension"
+echo "******************************"
+set +e
+printf "yes\n" | pecl install memcached
+set -e
+echo "Finished installing memcached extension."
+
+
+echo ""
+echo "******************************"
+echo "Installing phpredis extension."
+echo "******************************"
+echo ""
+echo ""
+echo "Downloading..."
+git clone git://github.com/phpredis/phpredis.git
+echo "Configuring..."
+cd phpredis
+phpize
+./configure
+echo "Installing..."
+make
+make install
+cd ..
+rm -Rf phpredis
+echo "Finished installing phpredis extension."
+
+
+echo ""
+echo "******************************"
+echo "Installing uopz extension."
+echo "******************************"
+set +e
+pecl install uopz
+set -e
+echo "Finished installing uopz extension."
+
+
+if [ -f "tests/travis/php_extensions.ini" ]
 then
   echo ""
   echo "*********************"
@@ -76,5 +78,5 @@ then
   echo "*********************"
   echo ""
   echo ""
-  phpenv config-add "tests/travis/php_extensions_${TRAVIS_PHP_VERSION}.ini"
+  phpenv config-add "tests/travis/php_extensions.ini"
 fi
